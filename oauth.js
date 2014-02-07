1725684374b14f00a9a2e1dac7939c28419dc4ce
dong wang <don_jc123@hotmail.com>
2014-02-06 22:33:52 -0500

    changes

--- oauth.js
+++ oauth.js
@@ -2,16 +2,16 @@ var ids = {
 facebook: {
  clientID: '642108929180162',
  clientSecret: 'a0d6f8b177e56b72f97c33fffad8e081',
- callbackURL: 'http://secret-stream-8223.herokuapp.com/auth/facebook/callback'
+ callbackURL: 'http://go306.herokuapp.com/auth/facebook/callback'
 },
 twitter: {
  consumerKey: 'vZV7avN1XxbA1nyJF74dIw',
  consumerSecret: 'lK796HVGw1SfNIOmf28SMDgaRZqHjPPwvN1jjQCvN4c',
- callbackURL: "http://secret-stream-8223.herokuapp.com/auth/twitter/callback"
+ callbackURL: "http://go306.herokuapp.com/auth/twitter/callback"
 },
 google: {
- returnURL: 'http://secret-stream-8223.herokuapp.com/auth/google/callback',
- realm: 'http://secret-stream-8223.herokuapp.com'
+ returnURL: 'http://go306.herokuapp.com/auth/google/callback',
+ realm: 'http://go306.herokuapp.com'
 }
 }
 
